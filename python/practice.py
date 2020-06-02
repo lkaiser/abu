936af93c873aeca02fd2e9a75edcc0a34935465e
minijjlk <minijjlk@gmail.com>
2020-06-02 09:25:42 +0800

    shake strategy

--- python/practice.py
+++ python/practice.py
@@ -4,6 +4,8 @@ from abupy.CoreBu import ABuEnv
 from abupy.PickStockBu import KPickStockValue, KPickStockStrongShake
 from abupy.MarketBu.KSymbolFin import FinDataSource
 import copy
+import sys
+from abupy.TradeBu.KFinManager import KFinManager
 
 
 ABuEnv.g_market_target = EMarketTargetType.E_MARKET_TARGET_CN
@@ -11,12 +13,12 @@ ABuEnv.g_market_target = EMarketTargetType.E_MARKET_TARGET_CN
 def batchpick():
 
 
-    stock_pickers = [{'class': KPickStockValue,'first_choice':True,'start':'20180101','end':'20200526'},
-                     {'class': KPickStockStrongShake, 'first_choice': True,'start':'20180101','end':'20200526', 'short_range': 30,'short_scope':1.15,'long_scope':1.6}]
-    #stock_pickers = [{'class': AbuPickRegressAngMinMax,
+    #stock_pickers = [{'class': KPickStockValue,'first_choice':True,'start':'20180101','end':'20200526'},
+    #                 {'class': KPickStockStrongShake, 'first_choice': True,'start':'20180101','end':'20200526', 'short_range': 30,'short_scope':1.15,'long_scope':1.6}]
+    stock_pickers = [{'class': KPickStockValue,'first_choice':True,'start':'20180101','end':'20200526'}]
     #                  'threshold_ang_min': 0.0, 'reversed': False}]
-
-    choice_symbols = None
+    fin_manager = KFinManager()
+    choice_symbols = fin_manager.get_stock_basic().ts_code.values()
     benchmark = AbuBenchmark()
     capital = AbuCapital(1000000, benchmark)
 
@@ -28,7 +30,7 @@ def batchpick():
 
 if __name__ == '__main__':
     ABuEnv.g_data_fetch_mode = EMarketDataFetchMode.E_DATA_FETCH_FORCE_NET
-    #batchpick()
+    batchpick()
     #ABuEnv.enable_example_env_ipython()
-    benchmark = AbuBenchmark()
-    print(benchmark.kl_pd)
+    #benchmark = AbuBenchmark()
+    #print(benchmark.kl_pd)
