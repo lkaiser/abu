7baf77b04fd3e4a4572192297898961a732a312d
minijjlk <minijjlk@gmail.com>
2020-06-05 17:27:44 +0800

    shake strategy

--- abupy/PickStockBu/KPickStockStrongShake.py
+++ abupy/PickStockBu/KPickStockStrongShake.py
@@ -61,7 +61,6 @@ class KPickStockStrongShake(AbuPickStockBase):
                 dic['short_range_deg'] = round(ABuRegUtil.calc_regress_deg(kl.adj_close,show=False),4)
                 dic['short_range_shake'] = ((kl.high-kl.low)/kl.low).mean()
                 #benchmark_rise = ((d_index.close/d_index.pre_close-1)*100).round(2)
-
                 relation_arr = [round(corr_xy(df.iloc[-(self.short_range+i):-i].rise.values, df.iloc[-(self.short_range+i):-i].benchmark_rise.values, ECoreCorrType.E_CORE_TYPE_PEARS), 4) for i in range(1,20)]
                 dic['short_range_relation'] = min(relation_arr) #前20天的relaition 取最小值，因为 relation主要是取30天的，已经做了平滑处理，再取前20天是因为信号不是一出来就有机会，后续需要根据位置判断
                 #dic['short_range_relation'] = round(corr_xy(kl.rise,benchmark_rise,ECoreCorrType.E_CORE_TYPE_PEARS),4)
